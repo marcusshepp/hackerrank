d6be0d636c736428ae909d5df2f8e32e08c13629
marcuss <marcuss@hesonline.com>
2016-06-03 13:16:40 -0400

    fiddling w ruby

---
+++ ruby/time_in_words.rb
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/the-time-in-words
+h = gets.chomp.to_i
+m = gets.chomp.to_i
+string = ""
+hour_map = {
+  1  => "one",
+  2 => "two",
+  3 => "three",
+  4 => "four",
+  5 => "five",
+  6 => "six",
+  7 => "seven",
+  8 => "eight",
+  9 => "nine",
+  10 => "ten",
+  11 => "eleven",
+  12 => "twelve"
+}
+
